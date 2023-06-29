d0c9500d44597fa2380169012d5fecfce4a9d3f4
hyunghung <97567582+hyunghung@users.noreply.github.com>
2023-06-29 11:55:31 -0700

    comparing inputs

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 var computerOptions = "RPS"
 
 function rps() {
-  for (let i = 0; i < 1; i++ ) {
     var computerChoice = computerOptions.charAt(Math.floor(Math.random() * computerOptions.length));
     console.log(computerChoice);
-  }
 }
 
-rps()
+
+var userInput = prompt("Choose R, P, or S");
+
+rps();
+
+function compare (userInput,rps() ){
+  if (userInput === rps() ){
+    alert("Tie");
+  }
+  else if (userInput === "R" && rps() === "P"){
+    alert("You lose");
+  }
+  else if (userInput === "R" && rps() === "S"){
+    alert("You won")
+  }
+  else if (userInput === "P" && rps() === "S"){
+    alert("You lose");
+  }
+  else if (userInput === "P" && rps() === "R"){
+    alert("You won")
+  }
+  else if (userInput === "S" && rps() === "R"){
+    alert("You lose");
+  }
+  else if (userInput === "S" && rps() === "P"){
+    alert("You won")
+  }
+}

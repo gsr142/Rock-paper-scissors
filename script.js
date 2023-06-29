e09b662a6c8e2160046422aabed8b097025f5a73
gsr142 <gsr142@gmail.com>
2023-06-29 12:19:31 -0700

    rename functions

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 var computerOptions = "RPS"
 
-function rps() {
-    var computerChoice = computerOptions.charAt(Math.floor(Math.random() * computerOptions.length));
-    console.log(computerChoice);
-}
+
 
 
 var userInput = prompt("Choose R, P, or S");
+userInput = userInput.toUpperCase();
 
-rps();
 
-function compare (userInput,rps() ){
-  if (userInput === rps() ){
+function compare (userInput, compChoice()){
+  if (userInput === compChoice()){
     alert("Tie");
   }
-  else if (userInput === "R" && rps() === "P"){
+  else if (userInput == "R" && compChoice() == "P"){
     alert("You lose");
   }
-  else if (userInput === "R" && rps() === "S"){
+  else if (userInput == "R" && compChoice() == "S"){
     alert("You won")
   }
-  else if (userInput === "P" && rps() === "S"){
+  else if (userInput == "P" && compChoice() == "S"){
     alert("You lose");
   }
-  else if (userInput === "P" && rps() === "R"){
+  else if (userInput == "P" && compChoice() == "R"){
     alert("You won")
   }
-  else if (userInput === "S" && rps() === "R"){
+  else if (userInput == "S" && compChoice() == "R"){
     alert("You lose");
   }
-  else if (userInput === "S" && rps() === "P"){
+  else if (userInput == "S" && compChoice() == "P"){
     alert("You won")
   }
 }
+
+function compChoice()() {
+  var computerChoice = computerOptions.charAt(Math.floor(Math.random() * computerOptions.length));
+  compare();
+  return computerChoice;
+}  
+
+compChoice()();
+//compare();
